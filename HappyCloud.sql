909314e4fbaabbc7ebb12ad3ae71f91095421a44
Patrice PETIT <patrice@coqsenpate.com>
2015-03-23 14:02:00 +0100

    Corrected syntax on MCD 2and 4.

--- HappyCloud.sql
+++ HappyCloud.sql
@@ -1,22 +1,30 @@
 # Databbase POSTGRESQL
 
 CREATE TABLE IF NOT EXISTS public."Organisation"(
-'ID_Organisation' SERIAL PRIMARY KEY,
-'organisationName' varchar(59) NOT NULL,
-'organisationEmail' varchar(50) NOT NULL
+	'ID_Organisation' SERIAL PRIMARY KEY,
+	'organisationName' varchar(59) NOT NULL,
+	'organisationEmail' varchar(50) NOT NULL
+	'activeOrganosation' boolean NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS public."UserAccount"(
-'ID_User' SERIAL PRIMARY KEY,
-'displayName' varchar(50) NOT NULL,
-'emailUser' varchar(50) UNIQUE NOT NULL,
-'password' varchar(50) NOT NULL,
-'accessLevel' integer NOT NULL
-'FK_ID_Organisation' INT NOT NULL
-'activeUserAccount'  boolean NOT NULL
-CONSTRAINT prk FOREIGN KEY FK_ID_Organisation REFERENCES public."Organisation"(ID_Organisation)
+	'ID_User' SERIAL PRIMARY KEY,
+	'displayName' varchar(50) NOT NULL,
+	'emailUser' varchar(50) UNIQUE NOT NULL,
+	'password' varchar(50) NOT NULL,
+	'accessLevel' integer NOT NULL
+	'FK_ID_Organisation' INT NOT NULL,
+	'activeUserAccount'  boolean NOT NULL,
+	CONSTRAINT prk FOREIGN KEY FK_ID_Organisation REFERENCES public."Organisation"(ID_Organisation)
 )
 
-CREATE TABLE IF NOT EXISTS public."Game"()
+CREATE TABLE IF NOT EXISTS public."Game"(
+	'ID_Game' SERIAL PRIMARY KEY,
+	'shortName' varchar(50)
+	'longName' varchar(50)
+	'version' varchar(50)
+	'path' varchar(50)
+	'metaData' Text;
+)
 CREATE TABLE IF NOT EXISTS public."Device"()
 CREATE TABLE IF NOT EXISTS public.""()
