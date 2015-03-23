7a4ffdc2bea7ea90f838ffd9bb783a69568ff4c4
Patrice PETIT <patrice@coqsenpate.com>
2015-03-23 16:31:01 +0100

    Added complete sqlscript.

--- HappyCloudV2.sql
+++ HappyCloudV2.sql
@@ -1,41 +1,51 @@
-# Databbase POSTGRESQL
+﻿CREATE TYPE Activity AS ENUM ('ACQUIRE','ASSIGN','CONNECT','CREATE','DISCONNECT','INSTALL','MODIFY','UPDATE');
 
 CREATE TABLE IF NOT EXISTS public."Organisation"(
-	'ID_Organisation' SERIAL PRIMARY KEY,
-	'name' varchar(59) NOT NULL,
-	'email' varchar(50) NOT NULL
-	'active' boolean NOT NULL
-)
+	"ID_Organisation" SERIAL PRIMARY KEY,
+	"name" varchar(50) NOT NULL,
+	"email" varchar(50) NOT NULL,
+	"active" boolean NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS public."UserAccount"(
-	'ID_User' SERIAL PRIMARY KEY,
-	'displayName' varchar(50) NOT NULL,
-	'email' varchar(50) UNIQUE NOT NULL,
-	'password' varchar(50) NOT NULL,
-	'accessLevel' integer NOT NULL
-	'FK_ID_Organisation' INT NOT NULL,
-	'active'  boolean NOT NULL,
-	CONSTRAINT prk FOREIGN KEY FK_ID_Organisation REFERENCES public."Organisation"(ID_Organisation)
-)
+	"ID_User" SERIAL PRIMARY KEY,
+	"display_Name" varchar(50) NOT NULL,
+	"email" varchar(50) UNIQUE NOT NULL,
+	"password" varchar(50) NOT NULL,
+	"access_Level" integer NOT NULL,
+	"FK_ID_Organisation" INT NOT NULL,
+	"active"  boolean NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS public."Game"(
-	'ID_Game' SERIAL PRIMARY KEY,
-	'shortName' varchar(50) NOT NULL,
-	'displayName' varchar(50) NOT NULL,
-	'version' varchar(50) NOT NULL,
-	'path' varchar(50) NOT NULL,
-	'metaData' Text NOT NULL,
-	'active' boolean NOT NULL,
-)
+	"ID_Game" SERIAL PRIMARY KEY,
+	"short_Name" varchar(50) NOT NULL,
+	"display_Name" varchar(50) NOT NULL,
+	"version" varchar(50) NOT NULL,
+	"path" varchar(50) NOT NULL,
+	"metaData" Text NOT NULL,
+	"active" boolean NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS public."Device"(
-	'UniqueIDDevice' varchar(50) PRIMARY KEY,
-	'plateFormType' varchar(50) NOT NULL,
-	'authentificationTokken' varchar(50) NOT NULL,
-	'active' boolean NOT NULL
-)
+	"Unique_ID_Device" varchar(50) PRIMARY KEY,
+	"plateForm_Type" varchar(50) NOT NULL,
+	"authentification_Tokken" varchar(50) NOT NULL,
+	"active" boolean NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS public."Activity"(
-	'ID_activity' SERIAL PRIMARY KEY,
-	'typeActivity' NOT NULL
-)
+	"ID_activity" SERIAL PRIMARY KEY,
+	"data_Activity" timeStamptz NOT NULL,
+	"type_Activity" Activity NOT NULL,
+	"FK_ID_Game" INT NOT NULL,
+	"FK_ID_Device" varchar(50) NOT NULL,
+	"FK_ID_User" INT NOT NULL,
+	"FK_ID_Organisation" INT NOT NULL
+);
+
+ALTER TABLE public."UserAccount" ADD CONSTRAINT "FK_ID_Organisation_UserAccount" FOREIGN KEY ("FK_ID_Organisation") REFERENCES public."Organisation"("ID_Organisation");
+ALTER TABLE public."Activity" ADD CONSTRAINT "FK_ID_Game_Activity" FOREIGN KEY ("FK_ID_Game") REFERENCES public."Game"("ID_Game");
+ALTER TABLE public."Activity" ADD CONSTRAINT "FK_ID_Device_Activity" FOREIGN KEY ("FK_ID_Device") REFERENCES public."Device"("Unique_ID_Device");
+ALTER TABLE public."Activity" ADD CONSTRAINT "FK_ID_User_Activity" FOREIGN KEY ("FK_ID_User") REFERENCES public."UserAccount"("ID_User");
+ALTER TABLE public."Activity" ADD CONSTRAINT "FK_ID_Organisation_Activity" FOREIGN KEY ("FK_ID_Organisation") REFERENCES public."Organisation"("ID_Organisation");
